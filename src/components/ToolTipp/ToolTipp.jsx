98ee40c7a212787fc0d86c46f7c22712061c99e8
shafeeq <shafeeqap1986@gmail.com>
2025-03-10 10:05:14 +0530

    update image

--- src/components/ToolTipp/ToolTipp.jsx
+++ src/components/ToolTipp/ToolTipp.jsx
@@ -1,6 +1,6 @@
+import PropTypes from "prop-types";
 import { useState } from "react";
 import "./ToolTipp.css";
-import PropTypes from "prop-types";
 
 const ToolTipp = ({ children, text }) => {
   const [isVisible, setIsVisible] = useState(false);
